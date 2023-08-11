41d6f6f35a7b32e247f901f34cf8e22e595499f1
d-komissarchik <0667669609@ukr.net>
2023-08-11 15:16:02 +0300

    get video ids from youtube channel

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-KEY = ''
+import googleapiclient.discovery
+
+KEY = 'AIzaSyAgkB_zpAQg_ayoT0qVZVtyD7NNOodWPZA'
+CHANNEL_NAME = 't0digital'
+
+youtube = googleapiclient.discovery.build('youtube', 'v3', developerKey=KEY)
+
+def get_video_ids_from_channel(channel_name):
+    #получаем информацию о канале
+    response = youtube.channels().list(
+        part="contentDetails", #получаем только информацию о контенте
+        forUsername=channel_name
+    ).execute()
+    print(response)
+
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    videos = get_video_ids_from_channel(CHANNEL_NAME)
+    #print_videos_info(videos)
